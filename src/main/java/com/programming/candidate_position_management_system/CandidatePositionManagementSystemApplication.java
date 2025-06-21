5c26a9fdf8a3934938121fbafe19c9fca78eec48
Pruthviraj_AB <ext.pruthvirajbasannavar12128@xpressbees.com>
2025-06-21 22:13:04 +0530

    Project structure added

---
+++ src/main/java/com/programming/candidate_position_management_system/CandidatePositionManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.programming.candidate_position_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidatePositionManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidatePositionManagementSystemApplication.class, args);
+	}
+
+}
